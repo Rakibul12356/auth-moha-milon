a9ff14d0ee2498d0ce24acf2b8abe5c3831fc684
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-02-01 22:54:59 +0600

    create context

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
@@ -33,7 +33,7 @@ const Navbar = () => {
                             {navLinks}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">Auth Moha Milon</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
--- src/main.jsx
+++ src/main.jsx
@@ -9,25 +9,27 @@ import Root from "./component/Root";
 import Home from "./component/Home";
 import Login from "./component/Login";
 import Register from "./component/Register";
+import AuthProvider from "./provider/AuthProvider";
+
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<Root></Root> ,
-    children:[
+    element: <Root></Root>,
+    children: [
       {
-        path:'/',
-        element:<Home></Home>
+        path: '/',
+        element: <Home></Home>
       },
       {
-        path:'/login',
-        element:<Login></Login>
+        path: '/login',
+        element: <Login></Login>
 
       },
       {
-        path:"/register",
-        element:<Register></Register>
+        path: "/register",
+        element: <Register></Register>
       }
     ]
   },
@@ -35,7 +37,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+   <AuthProvider>
+   <RouterProvider router={router} />
+   </AuthProvider>
   </React.StrictMode>
 );
 
